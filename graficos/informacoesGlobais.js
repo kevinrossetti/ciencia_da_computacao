f8b15b8a2b98972210dbbce9be9f06043662b04e
kevinrossetti <105368440+kevinrossetti@users.noreply.github.com>
2024-10-24 10:22:43 -0300

    save

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -3,11 +3,12 @@ const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const pessoasConectadas = (dados.total_pessoas_conectadas) / 1e9
-    
+    const pessoasNoMundo = (dados.total_pessoas_mundo)/1e9
+    const horas = parseInt(dados.tempo_medio)
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
     
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma rede social e passam em média <span>${horas}</span> horas conectadas.`
     console.log(paragrafo)
 
     const container = document.getElementById('graficos-container')
