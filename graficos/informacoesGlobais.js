1aaf536a86431ec9d995557922e6e0235a76e2ed
kevinrossetti <105368440+kevinrossetti@users.noreply.github.com>
2024-11-21 10:27:03 -0300

    save

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,22 +1,19 @@
-async (paragrafo); {
-    const url = 'https://raw.githubusercontent.com/kevinrossetti/ciencia_da_computacao/refs/heads/main/base_de_dados/esportes-dados-globais.json'
-    
-    async function vizualizarInformacoesGlobais() {
-        const res = await fetch(url)
-        const pessoasConectadas = (dados.total_pessoas_conectadas) / 1e9
-        const pessoasNoMundo = (dados.total_pessoas_mundo)/1e9
-        const horas = parseInt(dados.tempo_medio)
-        const minutos = Math.round((dados.tempo_medio - horas) *100)
-        const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo) *100).toFixed(2)
-        const paragrafo = document.createElement('p')
-        paragrafo.classList.add('graficos-container__texto')
-        
-        paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma rede social e passam em média <span>${horas}</span> horas e <span>${minutos}</span> minutos-conectadas.
-        <br> Isso significa que aproximadamente ${porcentagemConectada}% de pessoas estão conectadas em alguma rede social.`
-        console.log(paragrafo)
-    
-        const container = document.getElementById('graficos-container')
-        container.appendChild(paragrafo)
+const url = 'https://raw.githubusercontent.com/kevinrossetti/ciencia_da_computacao/refs/heads/main/base_de_dados/esportes-dados-globais.json'
+
+async function vizualizarInformacoesGlobais() {
+    const res = await fetch(url)
+    const dados = await res.json()
+    const praticamRegularmente = (dados.praticam_regularmente_esportes/ 1e9)
+    const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
+    const horas = parseInt(dados.tempo_medio_semana_praticando_esportes)
+    const porcentagemEsportes = ((praticamRegularmente / pessoasNoMundo ) * 100).toFixed(2)
+    const pessoasAssitem = (dados.total_pessoas_que_assistem_esportes/ 1e9)
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `No mundo dos esportes, à nível global onde temos <span>${pessoasNoMundo} bilhões</span> de pessoas, das quais <span>${praticamRegularmente} bilhões</span> de pessoas praticam esportes regularmente, onde despendem aproximadamente <span>${horas} horas</span> semanais para praticar esportes.<br>Isso significa que aproximadamente <span>${porcentagemEsportes}%</span> das pessoas praticam algum esporte. Já as pessoas que assistem esportes, gira em média de <span>${pessoasAssitem} bilhões</span> de pessoas`
+
+    const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo)
 }
-    vizualizarInformacoesGlobais()
-    }
+
+vizualizarInformacoesGlobais()
